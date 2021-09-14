813244199fa153e689cad8deef31ebb2b7e4d167
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-09-14 18:40:47 +0900

    #3.0 Class Components and State

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # movie App
 
 React JS Fundamentals Course (start 21.9.12~)
+ 
+
+Record of studying REACT
+https://www.notion.so/aa281282cc4f4328ab1406c28a154254?v=8586113ee82c4a9bb080441a0e9c5512
+
+
+Records of studying in Underdog Revolution with four team members in a React study
+https://udr.oopy.io/d7cb30ff-60ef-43f8-a9db-4343aeda52c1
--- src/App.js
+++ src/App.js
@@ -1,40 +1,27 @@
 import React from "react"
 import PropTypes from "prop-types"
-function Food({name, index, rating}) {  
-  return <h1>I like {name}, index={index}, rating={rating}/5.0</h1>;
-}
 
+class App extends React.Component {
+  state = {
+    count: 0
+  };
+  //add와 minus는 자바스크립트!
+  add = () => {
+    console.log("add");
+  };
+  minus = () => {
+    console.log("minus");
+  };
 
-Food.propTypes = {
-name: PropTypes.string.isRequired,
-index: PropTypes.number.isRequired,
-rating: PropTypes.number.isRequired,
-}
-const foodILike = [ 
-{
-  id:1,
-    name: "potato",
-    index: 1,
-    rating: 4.9,
-},
-{
-  id:2,
-  name: "chocolate",
-  index: 2,
-  rating: 4.8,
-}]
-
-function renderFood(dish) {
-  console.log(dish)
-  return <Food key={dish.id} name={dish.name} index={dish.index} rating={dish.rating}/>
-}
-function App() {
-  return (
-    <div>
-        {foodILike.map(renderFood)}
-    </div>
-    
-  );
+  render() {
+    return (
+      <div>
+        <h1>The number is {this.state.count}</h1>
+        <button onClick={this.add}>Add</button>
+        <button onClick={this.minus}>Minus</button>
+      </div>
+    );
+  }
 }
 
 export default App;
