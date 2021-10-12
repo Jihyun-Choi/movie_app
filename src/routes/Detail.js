d2b2bd1ed801b18fcaba815c00d1afee177ca995
Jihyun-Choi <70438098+Jihyun-Choi@users.noreply.github.com>
2021-10-12 16:47:11 +0900

    modify Detail.js

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Detail.css";
 
 class Detail extends React.Component {
   componentDidMount() {
@@ -9,8 +10,19 @@ class Detail extends React.Component {
   }
   render() {
     const { location } = this.props;
-    if (location.state) {
-      return <span>{location.state.title}</span>;
+    if (location.state) { 
+      return (
+        <div className="detail__container">
+          <h2>{location.state.title}</h2>    
+          <span>{location.state.year} / </span>         
+          <span>{location.state.genres}</span> 
+                        
+          <span>{location.state.summary}</span> 
+          <span>{location.state.poster}</span>    
+          <span>{location.state.medium_cover_image}</span>         
+       </div>  
+      );
+
     } else {
       return null;
     }
